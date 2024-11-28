e37152845cb76a86d833824c5bb173631de3fcbf
jawid-fakhari <jawid_fakhari@live.it>
2024-11-28 18:21:38 +0100

    reset model per iniziare uniform

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -4,5 +4,6 @@ varying float vRandom;
 
 void main()
 {
-    gl_FragColor = vec4(vRandom, vRandom, 1.0, 1.0);
+    // gl_FragColor = vec4(vRandom, vRandom, 1.0, 1.0);
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -6,21 +6,21 @@ attribute vec3 position;
 attribute float aRandom;
 
 // how to use varying to send data to fragment shader file.
-varying float vRandom;
+// varying float vRandom;
 
 void main()
 {
     // scrivendo gl_Position in questo modo ci da più controllo
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1; // per esempio creare onde sin sull'asse z.
-    modelPosition.z += aRandom * 0.1; // qui prendiamo il nostro attribute che abbiamo creato e li collochiamo sull'asse z.
+    //modelPosition.z += aRandom * 0.1; // qui prendiamo il nostro attribute che abbiamo creato e li collochiamo sull'asse z.
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
 
-    vRandom = aRandom;
+    // vRandom = aRandom;
     // ==
     // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
