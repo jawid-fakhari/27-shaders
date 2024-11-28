de02672447f0fa2cfce6a211a78b43a20e0927ab
jawid-fakhari <jawid_fakhari@live.it>
2024-11-28 17:31:36 +0100

    scrivere un altro modo vertex shader

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -11,6 +11,6 @@ void main()
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
-     // ==
+    // ==
     // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
