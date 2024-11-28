22e83c549f0a5e003eab05b747126607032c5feb
jawid-fakhari <jawid_fakhari@live.it>
2024-11-28 17:37:35 +0100

    modificare semplicemente vertex shader glsl

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -6,11 +6,16 @@ attribute vec3 position;
 
 void main()
 {
+    // scrivendo gl_Position in questo modo ci da più controllo
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1; // per esempio creare onde sin sull'asse z.
+    
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
+
+    
     // ==
     // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
