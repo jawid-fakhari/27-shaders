6b7ca7b74c2b06dad1de963fd0fad3a80735eb2f
jawid-fakhari <jawid_fakhari@live.it>
2024-11-28 17:31:14 +0100

    scrivere un altro modo vertex shader

--- src/script.js
+++ src/script.js
@@ -33,10 +33,10 @@ const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
  */
 // Material
 const material = new THREE.RawShaderMaterial({
-  // usiamo back quote `` per scrivere del codice dentro perché altrimenti dobbiamo scrivere tutto il codice su una riga
-  vertexShader: testVertextShader,
-  fragmentShader: testFragmentShader,
-  //   wireframe: true,
+    // usiamo back quote `` per scrivere del codice dentro perché altrimenti dobbiamo scrivere tutto il codice su una riga
+    vertexShader: testVertextShader,
+    fragmentShader: testFragmentShader,
+    //   wireframe: true,
 });
 
 // Mesh
@@ -47,22 +47,22 @@ scene.add(mesh);
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 };
 
 window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
 /**
@@ -70,10 +70,10 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100
 );
 camera.position.set(0.25, -0.25, 1);
 scene.add(camera);
@@ -86,7 +86,7 @@ controls.enableDamping = true;
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+    canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -97,16 +97,16 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const clock = new THREE.Clock();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+    const elapsedTime = clock.getElapsedTime();
 
-  // Update controls
-  controls.update();
+    // Update controls
+    controls.update();
 
-  // Render
-  renderer.render(scene, camera);
+    // Render
+    renderer.render(scene, camera);
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
 };
 
 tick();
